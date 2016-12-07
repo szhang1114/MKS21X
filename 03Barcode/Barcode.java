f9abdcf854e18886910b6efa5e9e27ee2a7f7c52
Steven Zhang <szhang9@stuy.edu>
2016-12-07 06:35:37 -0500

    barcode

---
+++ 03Barcode/Barcode.java
@@ -0,0 +1,14 @@
+public class Barcode implements Comparable<Barcode>{
+
+    private String _zip;
+    private int _checkDigit;
+
+    public Barcode(String zip){
+	if(_zip.length() != 5 ||
+	   for(int index = 0; index < _zip.length(); index ++){
+	       ((int)_zip.charAt(index) > 57 || (int)_zip.charA< 48){
+		    false;
+	       }
+	   }
+	   )
+	   
