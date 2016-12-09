cd8bbd0d5a9f4443a3458913fb32a24303b85d40
Steven Zhang <szhang@stuy.edu>
2016-12-08 23:40:58 -0500

    barcode

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,14 +1,53 @@
 public class Barcode implements Comparable<Barcode>{
 
-    private String _zip;
-    private int _checkDigit;
+    private String zip;
 
     public Barcode(String zip){
-	if(_zip.length() != 5 ||
-	   for(int index = 0; index < _zip.length(); index ++){
-	       ((int)_zip.charAt(index) > 57 || (int)_zip.charA< 48){
-		    false;
-	       }
-	   }
-	   )
+        try{Integer.parseInt(zip);
+	} catch (NumberFormatException e){
+	    System.err.println("Not a valid zip");
+	}
+	this.zip = zip;
+    }
+
+    private int checkSum(){
+	int sum = 0;
+	for(int index = 0; index < 5; index ++){
+	    sum += (int)zip.charAt(index);
+	}
+	System.out.println(sum);
+	return sum;
+    }
+	    
+
+	    
+    public String toString(){
+	String zip1 = zip + Integer.parseInt(zip)%10;
+	String code = zip + " |";
+	for(int index = 0; index < zip1.length(); index++){
+	    char current = zip1.charAt(index);
+	    switch (current){
+	    case '0': code += "||:::"; break;
+	    case '1': code += ":::||"; break;
+	    case '2': code += "::|:|"; break;
+	    case '3': code += "::||:"; break;
+	    case '4': code += ":|::|"; break;
+	    case '5': code += ":|:|:"; break;
+            case '6': code += ":||::"; break;
+	    case '7': code += "|:::|"; break;
+	    case '8': code += "|::|:"; break;
+	    case '9': code += "|:|::"; break;
+	    default: break;
+	    }
+	}
+	return code + "|";
+    }
+    public int compareTo(Barcode other){
+	return 1;
+    }
+
+  
+
+}
+		    
 	   
---
+++ 03Barcode/Driver.java
@@ -0,0 +1,11 @@
+public class Driver{
+    public static void main(String[]args) {
+	Barcode a = new Barcode("12346789");
+	Barcode b = new Barcode("85926");
+	System.out.println(a);
+	System.out.println(b);
+
+	Barcode c = new Barcode("a3df4");
+    }
+
+}
