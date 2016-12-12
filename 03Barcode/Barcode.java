40e5e7d4e68bb83f5b8b3c09448a2d3b65536a37
Steven Zhang <szhang@stuy.edu>
2016-12-11 22:04:07 -0500

    barcode

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -20,7 +20,14 @@ public class Barcode implements Comparable<Barcode>{
 	return sum;
     }
 
-    public static String toBarcode(String zip){
+    public static String toCode(String zip, int checkDigit){
+	if(zip.length() != 5){
+	    throw new IllegalArgumentException("zip is not the correct length");
+	}
+	try{Integer.parseInt(zip);
+	} catch (IllegalArgumentException e){
+	    System.err.println("zip contains characters other than digits");
+	}
 	String zip1 = zip + checkDigit;
 	String code = zip + " |";
 	for(int index = 0; index < zip1.length(); index++){
@@ -42,16 +49,67 @@ public class Barcode implements Comparable<Barcode>{
 	return code + "|";
     }
 
+    public static String codeToNum(String code){
+	String out = "";
+	for(int index = 0; index < code.length(); index += 5){
+	    String current = code.substring(index, index + 5);
+	    switch(current){
+	    case "||:::": out += "0"; break;
+	    case ":::||": out += "1"; break;
+	    case "::|:|": out += "2"; break;
+	    case "::||:": out += "3"; break;
+	    case ":|::|": out += "4"; break;
+	    case ":|:|:": out += "5"; break;
+	    case ":||::": out += "6"; break;
+	    case "|:::|": out += "7"; break;
+	    case "|::|:": out += "8"; break;
+	    case "|:|::": out += "9"; break;
+	    default: out = "invalid code"; break;
+	    }
+	}
+	return out;
+    }
+
+
+		
+	    
+
+    public static String toZip(String code){
+	if(code.length() != 32){
+	    throw new IllegalArgumentException("Length of barcode is not 32");
+	}
+	/*for(int counter = 0; counter < code.length(); counter ++){
+	    if(code.charAt(counter) == ':' || code.charAt(counter) == '|'){
+		throw new IllegalArgumentException("invalid barcode characters");
+	    }
+	    }*/
+	if(code.charAt(0) != '|' || code.charAt(31) != '|'){
+	    throw new IllegalArgumentException("first and last characters are not '|'");
+	}
+	if(codeToNum(code.substring(1,32)).compareTo("invalid code") == 0){
+	    throw new IllegalArgumentException("invalid values for barcode");
+	}
+	return codeToNum(code.substring(1,32));
+    }
+	
+
 	    
     public String toString(){
-	return toBarcode(this.zip, this.checkDigit);
+	return  toCode(this.zip, this.checkDigit);
     }
 
     public int compareTo(Barcode other){
 	return this.zip.compareTo(other.zip);
     }
-
-  
+    /*public static void main(String[]args) {
+	//Barcode a = new Barcode("12346789");
+	Barcode b = new Barcode("85926");
+	//System.out.println(a);
+	System.out.println(b);
+	String code = "||:::|::|::|::|:|:|::::|||::|:";
+	System.out.println(codeToNum(code));
+	Barcode c = new Barcode("a3df4");
+	}*/
 
 }
 		    
--- 03Barcode/Driver.java
+++ 03Barcode/Driver.java
@@ -4,7 +4,8 @@ public class Driver{
 	Barcode b = new Barcode("85926");
 	System.out.println(a);
 	System.out.println(b);
-
+	String code = "|||:::|::|::|::|:|:|::::|||::|:|";
+	System.out.println(toZip(code));
 	Barcode c = new Barcode("a3df4");
     }
 
